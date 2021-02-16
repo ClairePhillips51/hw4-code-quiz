53a341d3662680f3c1fa331a2ca4da88c31664dd
Claire Phillips <claireacam@gmail.com>
2021-02-15 20:26:07 -0500

    set up basic sturcture for html and css, added sample questions to javascipt.

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+let qustions = [
+    new Question("At the end of which HTML element does the <script> tag go?", ["bead", "body","html", "It goes in the css"], "body")
+    new Question("What is the basic structure for a for loop?", ["for(i=0; i<array.length; i++)", "for(i=== array)", "for (i==1 i++)", "for === false"] "for(i=0; i<array.length; i++)")
+]
